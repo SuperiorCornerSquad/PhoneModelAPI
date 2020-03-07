d1840887166c3edc71a5b6bd7bbebfdb1cdea50d
Joel <joel.lainebest@gmail.com>
2020-03-07 19:12:24 +0200

    dotenv dependecy

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const util = require("util");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mysql = require("mysql");
+require("dotenv").config();
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "apiuser",
-    password: "userapi",
-    database: "phonemodelapi"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 const query = util.promisify(con.query).bind(con);
 
