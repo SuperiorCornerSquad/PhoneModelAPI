c431c208b08d63de6b2e56638bfd972408d2d84c
Joel <joel.lainebest@gmail.com>
2020-03-09 17:26:08 +0200

    get routes

--- routes.js
+++ routes.js
@@ -22,4 +22,101 @@ router.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname+"/index.html"));
 });
 
+router.get("/api/v1/manufacturers", (req, res) => {
+	(async () => {
+		try {
+			const result = await query("SHOW TABLES");
+			const json = [];
+			for (let i in result) {
+				json.push(result[i].Tables_in_phonemodelapi);
+			}
+			res.json(json);
+		} catch(err) {
+			console.error("Database error: " + err);
+			res.sendStatus(500);
+		}
+	})();
+});
+
+router.get("/api/v1/manufacturers/:mfr", (req, res) => {
+	console.log(req.params.mfr);
+	// Object.keys(req.query).length === 0 && req.query.constructor === Object
+});
+
+router.get("/api/v1/manufacturers/:mfr/:id", (req, res) => {
+
+});
+
+router.get("/api/v1/smartphones", (req, res) => {
+
+	async function getManufacturers() {
+		try {
+			let sqlQuery = "SELECT DISTINCT table_name FROM information_schema.columns WHERE column_name ='Category'";
+			const result = await query(sqlQuery);
+			return result;
+		} catch(err) {
+			console.error("Database error: " + err);
+			res.sendStatus(500);
+		}
+	}
+
+	async function getPhones(tables) {
+		try {
+			let sqlQuery = "";
+			for (let i=0; i<tables.length; i++) {
+				sqlQuery = sqlQuery.concat("SELECT * FROM "+tables[i].table_name+" WHERE Category='smartphone' ");
+				if(i !== tables.length-1) {
+					sqlQuery = sqlQuery.concat("UNION ");
+				}
+			}
+			const result = await query(sqlQuery);
+			res.json(result);
+		} catch(err) {
+			console.error("Database error: " + err);
+			res.sendStatus(500);
+		}
+	}
+
+	(async () => {
+		let tables = await getManufacturers();
+		await getPhones(tables);
+	})();
+});
+
+router.get("/api/v1/phablets", (req, res) => {
+
+	async function getManufacturers() {
+		try {
+			let sqlQuery = "SELECT DISTINCT table_name FROM information_schema.columns WHERE column_name ='Category'";
+			const result = await query(sqlQuery);
+			return result;
+		} catch(err) {
+			console.error("Database error: " + err);
+			res.sendStatus(500);
+		}
+	}
+
+	async function getPhablets(tables) {
+		try {
+			let sqlQuery = "";
+			for (let i=0; i<tables.length; i++) {
+				sqlQuery = sqlQuery.concat("SELECT * FROM "+tables[i].table_name+" WHERE Category='phablet' ");
+				if(i !== tables.length-1) {
+					sqlQuery = sqlQuery.concat("UNION ");
+				}
+			}
+			const result = await query(sqlQuery);
+			res.json(result);
+		} catch(err) {
+			console.error("Database error: " + err);
+			res.sendStatus(500);
+		}
+	}
+
+	(async () => {
+		let tables = await getManufacturers();
+		await getPhablets(tables);
+	})();
+});
+
 module.exports = router;
