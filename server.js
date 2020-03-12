ba56594d6af18e2660f9f15b3af79ef1bca94a2c
Joel <joel.lainebest@gmail.com>
2020-03-12 12:07:17 +0200

    moved deletes and updates into own files

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 const morgan = require("morgan");
 const getRouter = require("./getRoutes");
 const postRouter = require("./postRoutes");
+const updateRouter = require("./updateRoutes");
+const deleteRouter = require("./deleteRoutes");
 
 app.use(morgan("short"));
 app.use(getRouter);
 app.use(postRouter);
+app.use(updateRouter);
+app.use(deleteRouter);
 app.use(express.static(__dirname + '/'));
 
 app.listen(8081, "localhost",  () => {
